021f034037da918f28c348207b6b914c51d8f821
Abhishek Sura <abhisheksura90@gmail.com>
2020-04-28 00:35:52 +0530

    Minimum Cost for Climibing Stairs
    LeetCode #746 - Dynamic Programming

---
+++ Dynamic Programming/MinCostClimbingStairs.cpp
@@ -0,0 +1,26 @@
+/*
+    LeetCode #746 - Min Cost Climbing Stairs
+    https://leetcode.com/problems/min-cost-climbing-stairs/
+    
+    Time Complexity - O(N)
+    Space Complexity - O(N)
+    
+    This is similar to Fibonacci Series and Space Complexity can be reduced to O(1)
+    Dynamic Programming - Bottom Up Approach
+*/
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int dp[n];
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < cost.size(); i++) {
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i];
+        }
+        return min(dp[n-1], dp[n-2]);
+    }
+};
