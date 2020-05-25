34be5da673a648e8b7ff0fc50b93ebc53da2f422
Abhishek Sura <abhisheksura90@gmail.com>
2020-05-26 00:12:02 +0530

    Linked List beginning of a cycle - LeetCode #142

--- Linked Lists/LinkedListCycle.cpp
+++ Linked Lists/LinkedListCycle.cpp
@@ -4,7 +4,7 @@
     
     Time Complexity - O(N)
     Space Complexity - O(1)
-    Hare - Tortoise / 2 Pointer Technique  
+    Hare - Tortoise / 2 Pointer Technique
 */
 
 class Solution {
@@ -24,5 +24,42 @@ public:
             }
         }
         return false;
+    }
+
+/*
+    LeetCode #142 - Linked List Beginning of Cycle
+    https://leetcode.com/problems/linked-list-cycle-ii/
+    Hare - Tortoise / 2 Pointer Technique
+*/
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL or head->next == NULL) {
+            return NULL;
+        }
+        ListNode *slow, *fast;
+        
+        slow = fast = head;
+        
+        while (fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if (fast == slow) {
+                break;
+            }
+        }
+
+        if (slow != fast) {
+            return NULL;
+        }
+        fast = head;
+        while (1) {
+            if (slow == fast) {
+                return fast;
+            }
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return NULL;
     }
 };
